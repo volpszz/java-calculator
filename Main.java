dc084327c830ab09add2bade6d4eb35583a45db4
volpsz <76572284+volpszz@users.noreply.github.com>
2026-01-07 09:03:35 -0300

    Refactor calculator logic for clarity and structure

--- Main.java
+++ Main.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
 
 void main(){
-Scanner sc = new Scanner(System.in);
-double a;
-double b;
-char tipo;
-System.out.println("Bem vindo a calculadora!");
-System.out.println("Qual o primeiro numero? ");
-a = sc.nextDouble();
-System.out.println("Qual o segundo numero? ");
-b = sc.nextDouble();
-System.out.println("Qual operacao voce quer? (+ | - | d");
-tipo = sc.next().charAt(0);
-if (tipo == '+'){
-     System.out.println("sua soma deu: " + (a + b));
+    Scanner sc = new Scanner(System.in);
+    double a;
+    double b;
+    char tipo;
+    System.out.println("Bem vindo a calculadora!");
+    System.out.println("Qual o primeiro numero? ");
+    a = sc.nextDouble();
+    System.out.println("Qual o segundo numero? ");
+    b = sc.nextDouble();
+    System.out.println("Qual operacao voce quer? (+ | - | d");
+    tipo = sc.next().charAt(0);
+     if (tipo == '+'){
+         System.out.println("sua soma deu: " + (a + b));
 }
-else if (tipo == '-') {
-     System.out.println("sua subtracao deu: " + (a - b));
+    else if (tipo == '-') {
+         System.out.println("sua subtracao deu: " + (a - b));
 }
-else if (tipo == 'd') {
-     System.out.println("sua divisao deu: " + (a / b));
+    else if (tipo == 'd') {
+         System.out.println("sua divisao deu: " + (a / b));
 }
 sc.close();
 }
