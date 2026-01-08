3bdeb1cd3c415bc51f52366f6b04231ccb6bc3ae
volpsz <76572284+volpszz@users.noreply.github.com>
2026-01-08 18:13:47 -0300

    Implement loop for continuous calculations
    Refactored calculator to allow repeated operations until user decides to stop.

--- Main.java
+++ Main.java
@@ -1,18 +1,22 @@
 import java.util.Scanner;
 
-void main(){
+void main() {
     Scanner sc = new Scanner(System.in);
+    char continuar;
     double a;
     double b;
     char tipo;
     System.out.println("Bem vindo a calculadora!");
-    System.out.println("Qual o primeiro numero? ");
-    a = sc.nextDouble();
-    System.out.println("Qual o segundo numero? ");
-    b = sc.nextDouble();
-    System.out.println("Qual operacao voce quer? (+ | - | d)");
-    tipo = sc.next().charAt(0);
-    switch(tipo){
+
+    do {
+        System.out.println("Qual o primeiro numero? ");
+        a = sc.nextDouble();
+        System.out.println("Qual o segundo numero? ");
+        b = sc.nextDouble();
+        System.out.println("Qual operacao voce quer? (+ | - | d)");
+        tipo = sc.next().charAt(0);
+
+    switch (tipo) {
         case '+':
             System.out.printf("Seu resultado e: %.2f%n", a + b);
             break;
@@ -26,10 +30,9 @@ void main(){
             System.out.println("Operaçao invalida!");
             break;
     }
-    sc.close();
+    System.out.println("Deseja continuar?");
+    continuar =  sc.next().toLowerCase().charAt(0);
+    } while (continuar == 's');
+       System.out.println("Encerrando Calculadora!");
+       sc.close();
 }
-
-
-
-
-
