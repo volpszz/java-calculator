642b950704ee5e2e0e6c96838e49bae27f4a0367
volpsz <76572284+volpszz@users.noreply.github.com>
2026-02-28 19:27:34 -0300

    Add files via upload

---
+++ calculus/Calculus.java
@@ -0,0 +1,36 @@
+package calculus;
+
+public class Calculus {
+
+    // add
+    public static double add(double a, double b) {
+        return a + b;
+    }
+
+    // subtract
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
+
+    // divide
+    public static double divide(double a, double b){
+        if (b == 0){
+            throw new ArithmeticException("Error, Division by zero! ");
+        }
+        else {
+            return a / b;
+        }
+    }
+    // multiply
+    public static double multiply(double a, double b){
+        return a * b;
+    }
+    // showResult
+    public static void showResult(double result){
+        System.out.printf("Result: ",result);
+    }
+
+
+}
+
+
